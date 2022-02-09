cefae8d1a8e945bca29993938d482165089fa1bf
v_tpfengcai <v_tpfengcai@tencent.com>
2022-02-09 11:05:01 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    title: "itcjf blog",
+    description: "itcjf blog vuepress",
+    base: '/',
+    themeConfig: {
+        searchMaxSuggestions: 10,
+        algolia: {
+            appId: "AI5QOZ7827",
+            apiKey: "10306174b4cb710fe9de532f1d364329",
+            indexName: "srbmp-dev",
+            algoliaOptions: {
+                hitsPerPage: 10,
+                facetFilters: ""
+            }
+        },
+        sidebar: [
+            '/',
+            '/itcjf/index',
+        ],
+       
+    },
+    plugins: [
+        ["@tencent/vuepress-plugin-element-tabs"],
+        [
+            'fulltext-search',
+            {
+                tokenize: 'full',
+                split: /\s+/,
+                encode: 'icase',
+            },
+        ],
+    ]
+};
---
+++ docs/itcjf/index.md
@@ -0,0 +1,1 @@
+# itcjf blog index
